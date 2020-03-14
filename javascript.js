b50bdd4301fa0ce6e56876c6cb1e8e1bb563e9da
Damian Tapp <dam180@hotmail.com>
2020-03-14 12:52:30 +1100

    changes done

--- javascript.js
+++ javascript.js
@@ -1,81 +1,145 @@
-var question = [
-  "There are 9 continents in the world.",
-  "There are no islands on Earth.",
-  "Ostriches have two toes on each leg.",
-  "Jason Day is a pro golfer.",
-  "Is a Nissan Silvia a drift car in Japan.",
-  "Golf balls predominatly green.",
-  "Australia has the most friendliest snakes in the world.",
-  "Does Lake Victoria in Uganda have the most violent thunderstorms in the world."
-];
-var options1 = [
-  "<button class=buttons1 onclick=q1i()>True</button><br /><br /><button class=buttons1 onclick=q1c()>False</button>"
-];
-var options2 = [
-  "<button class=buttons1 onclick=q1c()>False</button><br /><br /><buttons1 onclick=q1c()>True</button>"
-];
-function timer(){
-    c = c -1;
-    if (c) <200) {
-        timer.innerHTML = c;
-        window.clearInterval(update);
-        {
-            
-            update = setInterval("timer()", 100);
-        }
-    }
+// a countdown timer at the beginning of the quiz
+var Timer = 10;
+var checkTimer = false;
+var t;
+function countDown() {
+  document.getElementById("txt").value = Timer--;
+  +setInterval(countDown, 1000);
 }
-
-var a = 0;
-a++;
-var b = 0;
-b++;
-<text>Score: <text id="Score">0</text></text><br/>
-<text>Time: <text id="Timer">200</text></text><br/>
-
-message1.innerHTML = "Time's up!";
-
-function begin1() {
-  disappear1.innerHTML = "";
-  message1.innerHTML = question1[0];
-  message2.innerHTML = options1;
-  number1.innerHTML = a++;
-}
-
-function q1c() {
-  message3.innerHTML = "Correct";
-  message2.innerHTML = "";
-  score1.innerHTML = b++;
-  message4.innerHTML = "<button class=buttons1 onclick=next1()>Next</button>";
-}
-
-function q1i() {
-  message3.innerHTML = "Incorrect";
-  message2.innerHTML = "";
-  message4.innerHTML = "<button class=buttons2 onclick=next1()>Next</button>";
-}
-
-function next1() {
-  if (a == "2") {
-    message1.innerHTML = question1[1];
-    message2.innerHTML = options1;
-    message3.innerHTML = "";
-    number1.innerHTML = a++;
-    message4.innerHTML = "";
-  } else if (a == 3) {
-    message1.innerHTML = question1[2];
-    message2.innerHTML = options2;
-    message3.innerHTML = "";
-    number1.innerHTML = a++;
-    message4.innerHTML = "";
+function startTimer() {
+  if (!checkTimer) {
+    checkTimer = true;
   } else {
-    message1.innerHTML = "End of Quiz";
-    message2.innerHTML = "";
-    message3.innerHTML = "";
-    message4.innerHTML =
-      "<button class=buttons2 onclick=repeat1()>Repeat</button>";
+    console.log("Error!");
   }
 }
-function repeat1() {
-  location.reload();
+// this is to check only one box at a time..
+$("input:checkbox").on('click', function() {
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
+
+const myQuestions = [
+  {
+    question: "Is Jason Day a pro golfer?",
+    answers: {
+      a: "true",
+      b: "false",
+      C: "dont know"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Is a Nissan Silvia a drift car in Japan?",
+    answers: {
+      a: "true",
+      b: "false",
+      c: "dont know"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Toothpaste is a food?",
+    answers: {
+      a: "true",
+      b: "false",
+      c: "dont know"
+    },
+    correctAnswer: "b"
+  }
+];
+
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+function generateQuiz(
+  questions,
+  quizContainer,
+  resultsContainer,
+  submitButton
+);
+{
+  function showQuestions(questions, quizContainer){}; 
+  // a place to store the output and the answer choices
+  var output = [];
+  var answers;
+  // for each question...
+  for (var i = 0; i < questions.length; i++) {
+    // reset the list of answers for each available answer..
+    answers = [];
+  
+    for (letter in questions[i].answers) {
+      // add a html radio button
+      answers.push(
+        "<label>" +
+          '<input type="radio" name="question' +
+          i +
+          '"value ="' +
+          letter +
+          '">' +
+          letter +
+          ":" +
+          questions[i].answer[letter] +
+          "</label>"
+      );
+    }
+    // add this question and its answer to the output
+    output.push(
+      '<div class="question">' +
+        questions[i].question +
+        "</div>" +
+        '<div class="answers>"' +
+        answers.join("") +
+        "</div>"
+    );
+  }
+}
+// finally conbine our output list into one string of html and put in on the page
+quizContainer.innerHTML = output.join("");
+
+function showResults(questions, quizContainer, resultsContainer) {
+  // gather answer containers from the quiz
+  var answerContainer = quizContainer.querySelectorAll(".answers");
+  // keep track ofthe user's answers for each question in a loop
+  var userAnswer = "";
+  var numCorrect = 0;
+  for (var i = 0; i < questions.length; i++) {
+    // find selected answer
+    userAnswer = (
+      answerContainers[i].querySelector(
+        "input{name=question" + i + "]:checked"
+      ) || {}
+    ).value;
+    // add to the number of correct answers
+    if (userAnswer === questions[i].correctAnswer) {
+      numCorrect++;
+      answerContainers[i].style.color = "lightgreen";
+    } else {
+      answerContainers[i].style.color = "red";
+    }
+  }
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
+}
+// show questions right away
+showQuestions(questions, quizContainer);
+// on submit, show results
+submitButton.onclick = function() {
+  showResults(questions, quizContainer, resultsContainer);
+};
+// an area to put your name at the end of the quiz
+function validateForm() {
+  var x = document.forms["myForm"]["Fname"].value;
+  if (x == "") {
+    alert("name must be filled ot");
+    return false;
+  }
 }
